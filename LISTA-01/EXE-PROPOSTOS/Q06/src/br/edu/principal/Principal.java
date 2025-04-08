c9968918fc023a447206a49bd284b1864e491164
1berto <144922529+1bertoW@users.noreply.github.com>
2025-04-08 11:21:05 -0300

    Update Principal.java

--- LISTA-01/EXE-PROPOSTOS/Q06/src/br/edu/principal/Principal.java
+++ LISTA-01/EXE-PROPOSTOS/Q06/src/br/edu/principal/Principal.java
@@ -1,10 +1,23 @@
+
 package br.edu.principal;
+	import java.util.Scanner;
+		public class Principal {
+    
+				public static void main(String[] args) {
 
-public class Principal {
+        double salario, vendas;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o seu salário fixo: ");
+        salario = sc.nextDouble();
 
-	}
+        System.out.print("Digite o valor das vendas: ");
+        vendas = sc.nextDouble();
 
+        	double comissao = vendas * 4/100;
+        double salarioFinal = salario + comissao;
+
+        	System.out.println("Comissão, R$ " + comissao);
+        System.out.println("Salário final é, R$ " + salarioFinal);
+    }
 }
